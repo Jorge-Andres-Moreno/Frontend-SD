e9d07745159e49d49ccc981c9a9c5306b2e1e058
Jorge Andres Moreno Mera <jorgemoreno97@hotmail.com>
2020-05-13 03:38:42 +0000

    Format files: index.js and views index.html and add.html

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 #!/usr/bin/env node
 
 // Declare our dependencies
-var express = require('express');
-var request = require('superagent');
-var bodyParser = require('body-parser');
+var express = require("express");
+var request = require("superagent");
+var bodyParser = require("body-parser");
 
 //Defined IP backend
-var backendHost = process.env.BACKEND || '127.0.0.1';
+var backendHost = process.env.BACKEND || "127.0.0.1";
 
 //Define PORT
 var port = process.env.PORT || 80;
@@ -15,64 +15,67 @@ var port = process.env.PORT || 80;
 var app = express();
 
 //Define parse of objects that are receive and send
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Set the view engine to use EJS as well as set the default views directory
-app.use(express.static(__dirname + '/public'));
-app.set('views', __dirname + '/public/views');
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+app.use(express.static(__dirname + "/public"));
+app.set("views", __dirname + "/public/views");
+app.engine("html", require("ejs").renderFile);
+app.set("view engine", "html");
 
 //METHOD GET"/"" return index.html file and consult list of users
-app.get('/', function (req, res) {
-    console.log('GET="/"');
-    request.get('http://' + backendHost + ':8080/users')
-        .end(function (err, data) {
-            if (data == undefined) {
-                res.status(404).send({});
-            } else {
-                if (data.status != 200) {
-                    res.send(data.status, 'We have an bug');
-                } else {
-                    res.render('index', data.body);
-                }
-            }
-        })
+app.get("/", function (req, res) {
+  console.log('GET="/"');
+  request
+    .get("http://" + backendHost + ":8080/users")
+    .end(function (err, data) {
+      if (data == undefined) {
+        res.status(404).send({});
+      } else {
+        if (data.status != 200) {
+          res.send(data.status, "We have an bug");
+        } else {
+          res.render("index", data.body);
+        }
+      }
+    });
 });
 
 //METHOD POST"/"" return index.html file and consult list of users
-app.post('/', function (req, res) {
-    console.log('POST="/"');
-    //console.log(req.body)
-    request.post('http://' + backendHost + ':8080/user/add')
-        .end(function (err, data) {
-            if (data == undefined) {
+app.post("/", function (req, res) {
+  console.log('POST="/"');
+  //console.log(req.body)
+  request
+    .post("http://" + backendHost + ":8080/user/add")
+    .end(function (err, data) {
+      if (data == undefined) {
+        res.status(404).send({});
+      } else {
+        if (data.status != 200) {
+          res.send(data.status, "We have an bug");
+        } else {
+          request
+            .get("http://" + backendHost + ":8080/users")
+            .end(function (err, data) {
+              if (data == undefined) {
                 res.status(404).send({});
-            } else {
+              } else {
                 if (data.status != 200) {
-                    res.send(data.status, 'We have an bug');
+                  res.send(data.status, "We have an bug");
                 } else {
-                    request.get('http://' + backendHost + ':8080/users')
-                        .end(function (err, data) {
-                            if (data == undefined) {
-                                res.status(404).send({});
-                            } else {
-                                if (data.status != 200) {
-                                    res.send(data.status, 'We have an bug');
-                                } else {
-                                    res.render('index', data.body);
-                                }
-                            }
-                        })
+                  res.render("index", data.body);
                 }
-            }
-        })
+              }
+            });
+        }
+      }
+    });
 });
 
-app.get('/add', function (req, res) {
-    console.log('GET="/add"');
-    res.render('add');
+app.get("/add", function (req, res) {
+  console.log('GET="/add"');
+  res.render("add");
 });
 
 module.exports = app.listen(port);
